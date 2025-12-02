558bd13e4d83f364e1b9cc87a1ea283965b77717
vyex <vanessasoetjiadi@gmail.com>
2025-12-02 11:21:37 +1030

    Used shebang to make the CLI command easier to use

--- task-cli/cli.js
+++ task-cli/cli.js
@@ -1,12 +1,3 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
-
-const program = new Command();
-
-program
-  .name('my-cli')
-  .description('A CLI application built with commander.js')
-  .version('1.0.0');
-
-program.parse()
+console.log("Hello world");
