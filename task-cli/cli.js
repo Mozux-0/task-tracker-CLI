9829970861fb37c04863c10f6d7de2da00de4e91
vyex <vanessasoetjiadi@gmail.com>
2025-12-02 23:06:35 +1030

    Successfully added the delete function

--- task-cli/cli.js
+++ task-cli/cli.js
@@ -14,30 +14,6 @@ const cliCommands = [ 'add', 'update', 'delete' ]
 
 const input = process.argv[2]
 
-// delete later, for testing purposes
-// if (input.includes(generalCommands[6])) {
-//   const filePath = path.join(__dirname, 'tasks.json');
-
-//   const data = fs.readFileSync(filePath, 'utf-8');
-
-//   let arr = JSON.parse(data);
-
-//   arr.push({ name: "kontol" });
-
-//   fs.writeFileSync(filePath, JSON.stringify(arr, 2, null));
-
-//   console.log(arr);
-
-//   // fs.appendFileSync(filePath, JSON.stringify({ name: 'test' }, null, 2),'utf-8');
-//   // fs.appendFileSync(filePath, JSON.stringify({ name: 'kontl' }, null, 2),'utf-8');
-
-//   // fs.readFileSync(filePath, 'utf-8', (err, data) => {
-//   //   if (err) { console.log(err); };
-//   //   console.log('File data:', data);
-//   // });
-//   process.exit(0);
-// };
-
 if (input.includes(generalCommands[2]) || input.includes(generalCommands[3])) {
   console.log('task-cli v1.0.0');
   process.exit(0);
@@ -67,6 +43,20 @@ if (input.includes(generalCommands[4]) || input.includes(generalCommands[5])) {
   process.exit(0);
 };
 
+function validateId (id, maxCount, arr) {
+  if (id > maxCount || id <= 0) {
+    console.log(`Invalid ID number. Please enter an ID below ${ maxCount }`);
+    process.exit(1);
+  };
+
+  const exist = arr.items.some(obj => obj.id === id);
+
+  if (!exist) {
+    console.log(`Task ${ id } is not found, please input a valid id`);
+    process.exit(1);
+  }
+};
+
 const arg1 = process.argv[3]
 const arg2 = process.argv[4]
 const filePath = path.join(__dirname, 'tasks.json')
@@ -84,7 +74,7 @@ switch (input) {
     };
 
     arr.count += 1;
-    const currId = arr.count;
+    var currId = arr.count;
 
     arr.items.push({ id: currId, description: itemName, status: "todo" });
     fs.writeFileSync(filePath, JSON.stringify(arr, null, 2), 'utf-8');
@@ -97,19 +87,8 @@ switch (input) {
       console.log("Invalid description. Use the following command: task-cli update <id> <new description>");
     };
 
-    const chosenId = Number(arg1), currSum = arr.count, newDescription = arg2;
-
-    if (chosenId > currSum || chosenId <= 0) {
-      console.log(`Invalid ID number. Please enter an ID below ${ currCount }`);
-      process.exit(1);
-    };
-
-    const exist = arr.items.some(obj => obj.id === chosenId);
-
-    if (!exist) {
-      console.log(`Task ${ chosenId } is not found, please input a valid id`);
-      process.exit(1);
-    }
+    var chosenId = Number(arg1), currSum = arr.count, newDescription = arg2;
+    validateId(chosenId, currSum, arr);
 
     arr.items[chosenId - 1].description = newDescription;
     fs.writeFileSync(filePath, JSON.stringify(arr, null, 2), 'utf-8');
@@ -117,7 +96,19 @@ switch (input) {
     console.log(`Task ${ chosenId } has successfully been updated to ${ newDescription }`);
     process.exit(0);
   case 'delete':
-    console.log(`Task ID is successfully deleted`);
+    var chosenId = Number(arg1), currSum = arr.count;
+    validateId(chosenId, currSum, arr);
+
+    arr.items.splice(chosenId - 1, 1);
+    arr.items.forEach((item, i) => {
+      item.id = i + 1;      
+    });
+
+    arr.count = arr.items.length;
+
+    fs.writeFileSync(filePath, JSON.stringify(arr, null, 2), 'utf-8');
+  
+    console.log(`Task ${ chosenId } is successfully deleted`);
     process.exit(0);
   default:
     console.log("Invalid command. Use -cl or --commandlist to see the valid commands.");
